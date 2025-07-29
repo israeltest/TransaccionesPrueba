f884b23cd1b8b491e52c9d92fa5daf397babfe97
isra <israel@>
2025-07-29 15:28:30 -0500

    Agregar archivos de proyecto.

---
+++ ApiProductos/Services/IProductoService.cs
@@ -0,0 +1,14 @@
+﻿using ApiProductos.DTOs;
+using ApiProductos.Models;
+
+namespace ApiProductos.Services
+{
+    public interface IProductoService
+    {
+        Task<IEnumerable<Producto>> ObtenerTodos();
+        Task<Producto?> ObtenerPorId(int id);
+        Task Crear(ProductoCreateDto dto);
+        Task Actualizar(ProductoUpdateDto dto);
+        Task Eliminar(int id);
+    }
+}
---
+++ ApiProductos/Services/ProductoService.cs
@@ -0,0 +1,51 @@
+﻿using ApiProductos.DTOs;
+using ApiProductos.Models;
+using ApiProductos.Repositories;
+
+namespace ApiProductos.Services
+{
+    public class ProductoService : IProductoService
+    {
+        private readonly IProductoRepository _repo;
+
+        public ProductoService(IProductoRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IEnumerable<Producto>> ObtenerTodos() => await _repo.ObtenerProductos();
+
+        public async Task<Producto?> ObtenerPorId(int id) => await _repo.ObtenerProductoPorId(id);
+
+        public async Task Crear(ProductoCreateDto dto)
+        {
+            var producto = new Producto
+            {
+                Nombre = dto.Nombre,
+                Descripcion = dto.Descripcion,
+                Categoria = dto.Categoria,
+                Imagen = dto.Imagen,
+                Precio = dto.Precio,
+                Stock = dto.Stock
+            };
+            await _repo.CrearProducto(producto);
+        }
+
+        public async Task Actualizar(ProductoUpdateDto dto)
+        {
+            var producto = new Producto
+            {
+                Id = dto.Id,
+                Nombre = dto.Nombre,
+                Descripcion = dto.Descripcion,
+                Categoria = dto.Categoria,
+                Imagen = dto.Imagen,
+                Precio = dto.Precio,
+                Stock = dto.Stock
+            };
+            await _repo.ActualizarProducto(producto);
+        }
+
+        public async Task Eliminar(int id) => await _repo.EliminarProducto(id);
+    }
+}
